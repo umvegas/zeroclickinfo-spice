54b597223227c17c14a3f2328de79ea884110425
Zaahir Moolla <moollaza@duckduckgo.com>
2015-05-28 19:16:28 +0000

    Create new endpoint for Stock Market Info

--- share/spice/stocks/stocks.js
+++ share/spice/stocks/stocks.js
@@ -8,48 +8,53 @@
 
         var url = "http://www.nasdaq.com/symbol/" + api_result.Security.Symbol + "/real-time";
 
-        DDG.require('moment.js', function(){
-            Spice.add({
-                id: 'stocks',
-                name: 'Stock',
-                data: api_result,
-                meta: {
-                    sourceName: 'NASDAQ',
-                    sourceUrl: url,
-                    attributionText: "Xignite"
-                },
-                normalize: function(data){
-                    var change = data.ChangeFromPreviousClose,
-                        changeDir;
-                    moment().utcOffset(data.UTCOffset);
+        $.getJSON('/js/spice/stock_market_status/', function(market_data){
 
-                    // remove +/- from change attributes and add up/down class:
-                    if (change > 0 ) {
-                        changeDir = 'up';
-                    } else if(change < 0) {
-                        changeDir = 'down';
-                    } else {
-                        changeDir = 'same';
-                    }
+            DDG.require('moment.js', function(){
+                Spice.add({
+                    id: 'stocks',
+                    name: 'Stock',
+                    data: api_result,
+                    meta: {
+                        sourceName: 'NASDAQ',
+                        sourceUrl: url,
+                        attributionText: "Xignite"
+                    },
+                    normalize: function(data){
+                        var change = data.ChangeFromPreviousClose,
+                            changeDir;
 
-                    return {
-                        url: url,
-                        urlTitle: 'View more ' + data.Security.Name + ' stock data at NASDAQ',
-                        quote: data.Last.toFixed(2),
-                        quoteChangeDir: changeDir,
-                        change: change.toFixed(2),
-                        change_percent: data.PercentChangeFromPreviousClose.toFixed(2),
-                        date: moment(data.Date).format("MMM DD"),
-                        time: moment(data.Time, "hh:mm:ss A").format("h:mm A")
-                    };
-                },
-                templates: {
-                    group: 'base',
-                    options: {
-                        content: Spice.stocks.content,
-                        moreAt: false
+                        moment().utcOffset(data.UTCOffset);
+
+                        // remove +/- from change attributes and add up/down class:
+                        if (change > 0 ) {
+                            changeDir = 'up';
+                        } else if(change < 0) {
+                            changeDir = 'down';
+                        } else {
+                            changeDir = 'same';
+                        }
+
+                        return {
+                            url: url,
+                            urlTitle: 'View more ' + data.Security.Name + ' stock data at NASDAQ',
+                            quote: data.Last.toFixed(2),
+                            quoteChangeDir: changeDir,
+                            change: change.toFixed(2),
+                            change_percent: data.PercentChangeFromPreviousClose.toFixed(2),
+                            date: moment(data.Date).format("MMM DD"),
+                            time: moment(data.Time, "hh:mm:ss A").format("h:mm A"),
+                            market_open: market_data.value
+                        };
+                    },
+                    templates: {
+                        group: 'base',
+                        options: {
+                            content: Spice.stocks.content,
+                            moreAt: false
+                        }
                     }
-                }
+                });
             });
         });
     };
